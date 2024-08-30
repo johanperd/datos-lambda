1863006c61d049f507ffd42db308db75c3cab1bb
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-08-30 14:45:22 -0500

    inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const logger = require('./logger'); 
+
+const app = express();
+
+const datos = [
+    { id: 1, nombre: 'pedro' },
+    { id: 2, nombre: 'juan' },
+    { id: 3, nombre: 'maria' },
+    { id: 4, nombre: 'karina' },
+    { id: 5, nombre: 'julieta' },
+];
+
+
+app.get('/datos', (req, res) => {
+    logger.info('Ruta /datos accedida, retornado datos {'+JSON.stringify(datos)+'}'); // Log de información
+    res.send(JSON.stringify(datos));
+});
+
+// Exporta el manejador para AWS Lambda
+module.exports.handler = serverless(app);
