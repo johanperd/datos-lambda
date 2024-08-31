31e7fac848243c1cec5c67f4dc79fa95dc56561f
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-08-30 22:13:06 -0500

    data

--- main.tf
+++ main.tf
@@ -3,20 +3,19 @@ provider "aws" {
 }
 
 # IAM Role for Lambda Execution (reutilizado)
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+#resource "aws_iam_role" "lambda_exec_role" {
+#  name = "lambda_exec_role"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [{
+#      Action = "sts:AssumeRole",
+#      Effect = "Allow",
+#      Principal = {
+#        Service = "lambda.amazonaws.com"
+#      }
+#    }]
+#  })
+#}
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
