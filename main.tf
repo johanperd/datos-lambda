2ec48944850e45e1c29e726f6edd3875e7096d96
Johan Perdomo <johanperd@gmail.com>
2024-09-02 14:50:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# IAM Role for Lambda Execution
-# resource "aws_iam_role" "lambda_exec_role" {
-#   name = "lambda_exec_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
- #    Statement = [{
-  #     Action = "sts:AssumeRole",
-   #    Effect = "Allow",
-    #   Principal = {
-     #    Service = "lambda.amazonaws.com"
-      # }
-   #  }]
-  # })
-# }
+ IAM Role for Lambda Execution
+ resource "aws_iam_role" "lambda_exec_role_1" {
+   name = "lambda_exec_role_1"
+   assume_role_policy = jsonencode({
+     Version = "2012-10-17",
+    Statement = [{
+       Action = "sts:AssumeRole",
+       Effect = "Allow",
+       Principal = {
+        Service = "lambda.amazonaws.com"
+       }
+     }]
+   })
+ }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
+#resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role_1.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "node_app_datos" {
   function_name = "node_app_datos"
   runtime       = "nodejs20.x"
   handler       = "app.handler"  # Nombre del archivo y exportación del manejador
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_exec_role_1.arn
   filename      = "node_app_datos.zip"
 
   source_code_hash = filebase64sha256("node_app_datos.zip")
