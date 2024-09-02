7148889a5105da041abe21067f9e4b51f160f35d
Johan Perdomo <johanperd@gmail.com>
2024-09-02 16:09:46 -0500

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,9 +1,9 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const logger = require('./logger'); 
 
 
@@ -11,52 +11,34 @@ const headers = {
   'Authorization': 'Basic OTg2MTA2OmdsY19leUp2SWpvaU1URTNOVE0yTmlJc0ltNGlPaUpoZDNNdFlYZHpMV3hoYldKa1lTSXNJbXNpT2lJMVJFUktORFk1VDA4eGVXaHdjVTh6VlRJM1NEWlBSRElpTENKdElqcDdJbklpT2lKMWN5SjlmUT09'
 };
 
-
-// Configura el nivel de diagnóstico (opcional)
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.TRACE);
-
-// Configura el exportador de trazas
 const traceExporter = new OTLPTraceExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/traces', // Cambia la URL según tu backend de OTLP
-    headers: headers
-
-  });
-
-// Configura el exportador de métricas
-const metricExporter = new OTLPMetricExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/metrics', // Cambia la URL según tu backend de OTLP
-    headers: headers
-
-  });
-
-// Configura el exportador de logs
-const logExporter = new OTLPLogExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/logs', // Cambia la URL según tu backend de OTLP
-    headers: headers
-
-  });
-
-// Configura el SDK de Node con los exportadores y las instrumentaciones automáticas
-const sdk = new NodeSDK({
-    traceExporter,
-    metricExporter,
-    logExporter,
-    instrumentations: [getNodeAutoInstrumentations()],
+  url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/traces',
+  headers: headers
 });
 
+const metricExporter = new OTLPMetricExporter({
+  url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/metrics',
+  headers: headers
+});
+
+const logExporter = new OTLPLogExporter({
+  url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/logs',
+  headers: headers
+});
+
+const sdk = new NodeSDK({
+  traceExporter: traceExporter,
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+    exportIntervalMillis: 10000 // Exportar métricas cada 10 segundos
+  }),
+  logExporter: logExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
 
 try {
-  sdk.start();
-  logger.info('Tracing initialized');
-} catch (error) {
-  logger.info('Error initializing tracing', error);
-}
-
-
-// Captura la finalización de la aplicación para limpiar el SDK
-process.on('SIGTERM', () => {
-    sdk.shutdown()
-        .then(() => console.log('OpenTelemetry SDK cerrado'))
-        .catch((error) => console.error('Error cerrando OpenTelemetry SDK', error))
-        .finally(() => process.exit(0));
-});
+    sdk.start();
+    logger.info('Tracing initialized');
+  } catch (error) {
+    logger.info('Error initializing tracing', error);
+  }
