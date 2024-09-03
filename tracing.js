dea450216e39711f9622f4d734b17572b1efa5be
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-09-02 23:38:55 -0500

    qerwe

--- tracing.js
+++ tracing.js
@@ -5,7 +5,8 @@ const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 const logger = require('./logger'); 
-
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 const headers = {
   'Authorization': 'Basic OTg2MTA2OmdsY19leUp2SWpvaU1URTNOVE0yTmlJc0ltNGlPaUpoZDNNdFlYZHpMV3hoYldKa1lTSXNJbXNpT2lJMVJFUktORFk1VDA4eGVXaHdjVTh6VlRJM1NEWlBSRElpTENKdElqcDdJbklpT2lKMWN5SjlmUT09'
@@ -17,28 +18,31 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.TRACE);
 
 // Configura el exportador de trazas
 const traceExporter = new OTLPTraceExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/traces', // Cambia la URL según tu backend de OTLP
+    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/traces', // Cambia la URL según tu backend de OTLP
     headers: headers
 
   });
 
 // Configura el exportador de métricas
 const metricExporter = new OTLPMetricExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/metrics', // Cambia la URL según tu backend de OTLP
+    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/metrics', // Cambia la URL según tu backend de OTLP
     headers: headers
 
   });
 
 // Configura el exportador de logs
 const logExporter = new OTLPLogExporter({
-    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/v1/logs', // Cambia la URL según tu backend de OTLP
+    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/logs', // Cambia la URL según tu backend de OTLP
     headers: headers
 
   });
 
 // Configura el SDK de Node con los exportadores y las instrumentaciones automáticas
 const sdk = new NodeSDK({
-    traceExporter,
+  
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'datos-dynamo'
+  }),    traceExporter,
     metricExporter,
     logExporter,
     instrumentations: [getNodeAutoInstrumentations()],
